358f51738bc457113dfb79eacb34286448e335d7
Savita Ravindra Shejwal <shejwalsavi2002@gmail.com>
2025-02-03 09:57:50 +0530

    Add files via upload

---
+++ LinkedinCloneUsingJSwithHTML,CSS/script.js
@@ -0,0 +1,21 @@
+let profileMenu = document.getElementById("profileMenu");
+
+function toggleMenu(){
+    profileMenu.classList.toggle("open-menu");
+}
+
+let sideActivity = document.getElementById("sidebarActivity");
+
+let moreLink = document.getElementById("ShowMoreLink")
+
+function toggleActivity(){
+    sideActivity.classList.toggle("open-activity");
+
+    if(sideActivity.classList.contains("open-activity")){
+        moreLink.innerHTML = "Show less <b>-</b>";
+    }
+    else{
+        moreLink.innerHTML = "Show less <b>+</b>";
+    }
+}
+
